fede8e2bd63c8fec667c78663b4153d8c86a5926
RosslynAsto <rosly_13_5@hotmail.com>
2022-09-08 13:49:31 -0500

    Subido 2

--- main.js
+++ main.js
@@ -1,43 +1,79 @@
 import{ data } from "./js/data.js";
-
+import Swal from 'https://cdn.jsdelivr.net/npm/sweetalert2@11/src/sweetalert2.js'
 const contentShoes = document.querySelector(".content_shoes");
 const cartShopping = document.querySelector(".cartShopping");
 const containerShopping = document.querySelector(".container__shopping");
 const contentShopping = document.querySelector(".content__shopping");
+const shoppingTotal = document.querySelector(".shoppingTotal");
 
-const shoppingObj = {};
+let shoppingObj =  JSON.parse(localStorage.getItem("shoppingObj")) || {}; 
+console.log(shoppingObj);
 
 contentShopping.addEventListener("click",(event)=>{
  if (event.target.classList.contains("rest")) {
      const id = parseInt(event.target.parentElement.id);
      if (shoppingObj[id].amount === 1) {
-        const res = confirm ("Seguro de Eliminar?")
-        if (res) {
-            delete shoppingObj[id];
-        } else{
-            shoppingObj[id].amount--;
-        }
+        Swal.fire({
+            title: 'Desea Eliminar?',
+            showCancelButton: true,
+            confirmButtonText: 'OK',
+            denyButtonText: `Cancelar`,
+          }).then((result) => {
+            if (result.isConfirmed) {            
+              delete shoppingObj[id];  
+                  
+            }else{
+                  shoppingObj[id].amount--;
+            }
+            amountProductInCart();
+            printTotalPrice();
+            printShoppingCart(); 
+          })
+        
+     }else{
+        shoppingObj[id].amount--;
      }
+     localStorage.setItem("shoppingObj",JSON.stringify(shoppingObj));
+     amountProductInCart();
+     printTotalPrice();
+     printShoppingCart();
  }
  if (event.target.classList.contains("add")) {
     const id = parseInt(event.target.parentElement.id);
     if (shoppingObj[id].stock > shoppingObj[id].amount) {
-        shoppingObj[id].amount++;             
+        shoppingObj[id].amount++;       
+        localStorage.setItem("shoppingObj",JSON.stringify(shoppingObj));  
      }else{
-        alert('Agotado');
+        Swal.fire('No hay stock')
      }
+     localStorage.setItem("shoppingObj",JSON.stringify(shoppingObj));
+     amountProductInCart();
+    printTotalPrice();
+    printShoppingCart();
 }
 if (event.target.classList.contains("del")) {
     const id = parseInt(event.target.parentElement.id);
-    const res = confirm ("Seguro de Eliminar?")
-    if (res) {
-        delete shoppingObj[id];
-    } else{
-        shoppingObj[id].amount--;
-    }
+    Swal.fire({
+        title: 'Desea Eliminar?',
+        showCancelButton: true,
+        confirmButtonText: 'OK',
+        denyButtonText: `Cancelar`,
+      }).then((result) => {
+        if (result.isConfirmed) {            
+          delete shoppingObj[id];  
+              
+        }else{
+              shoppingObj[id].amount--;
+        }
+        localStorage.setItem("shoppingObj",JSON.stringify(shoppingObj));
+        amountProductInCart();
+        printTotalPrice();
+        printShoppingCart(); 
+      })
 }
-printShoppingCart();
+ 
 });
+
 contentShoes.addEventListener("click",(event)=>{
  if (event.target.classList.contains("add_principal")) {
      const id = parseInt(event.target.parentElement.id);
@@ -45,18 +81,42 @@ contentShoes.addEventListener("click",(event)=>{
 
      if(shoppingObj[id]){
          if (shoppingObj[id].stock > shoppingObj[id].amount) {
-            shoppingObj[id].amount++;             
+            shoppingObj[id].amount++;    
+            localStorage.setItem("shoppingObj",JSON.stringify(shoppingObj));            
          }else{
-            alert('Agotado');
+            Swal.fire('No hay stock')
          }
-        
+          
      }else{
         shoppingObj[id] = currentProduct;
         shoppingObj[id].amount=1;
+        localStorage.setItem("shoppingObj",JSON.stringify(shoppingObj));
      }
+
+   
+
+   
+     amountProductInCart();
+     printTotalPrice();
      printShoppingCart();
  }
 });
+
+function amountProductInCart() {    
+    const infoQuatityProducts = document.querySelector(".infoQuatityProducts");
+    infoQuatityProducts.textContent = Object.values(shoppingObj).length;
+ }
+
+function printTotalPrice() {
+    const shoppingArray = Object.values(shoppingObj);
+     let suma =0;
+
+     shoppingArray.forEach((n)=>{
+        suma += n.amount * n.price;
+     });
+
+     shoppingTotal.textContent = suma;
+}
 
 function printShoppingCart(params) {
     const shoppingArray = Object.values(shoppingObj);
@@ -111,6 +171,9 @@ function printShoes(){
 }
 printShoes();
 
-cartShopping.addEventListener('click',()=>{
+printShoppingCart();
+amountProductInCart();
+printTotalPrice();
+cartShopping.addEventListener('click',()=>{       
     containerShopping.classList.toggle("show__shopping");
 });
